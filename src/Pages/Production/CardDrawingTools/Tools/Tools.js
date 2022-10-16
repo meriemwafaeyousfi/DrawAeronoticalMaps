fecfd8e22521bb0798878f1d37862e54faa58c1e
y.lalmi <y.lalmi@meteo.dz>
2022-10-16 16:09:34 +0100

    Fixes centreAction and updates  some functionalties in Tools

--- src/Pages/Production/CardDrawingTools/Tools/Tools.js
+++ src/Pages/Production/CardDrawingTools/Tools/Tools.js
@@ -1,397 +1,360 @@
-import React, { useCallback, useEffect, useState } from "react";
-import "./Tools.css";
-import { DragPan } from "ol/interaction";
-import UndoRedo from "ol-ext/interaction/UndoRedo";
+import React, { useCallback, useEffect, useState } from 'react';
+import './Tools.css';
+import { DragPan } from 'ol/interaction';
+import UndoRedo from 'ol-ext/interaction/UndoRedo';
 import {
-  translateOff,
-  translateOn,
-  endDrawing,
-  selectOff,
-  selectOn,
-  zoomingInAndCenter,
-  zoomingOutAndCenter,
-  dragPanOff,
-  save,
-} from "../../../../Mapping/Map";
-import { cloudDrawingON } from "../../../../Mapping/Features/Clouds/Clouds";
-import { jetFlowDrawingON } from "../../../../Mapping/Features/JetFlow/JetFlow";
-import { frontFlowDrawingON } from "../../../../Mapping/Features/FrontFlow/FrontFlow";
-import { useDispatch, useSelector } from "react-redux";
-import { setMapCoordinate, setModal, setOption } from "../redux/actions";
-import { jetDrawingON } from "../../../../Mapping/Features/Jet/Jet";
-import { centreActionDrawingON } from "../../../../Mapping/Features/CentreAction/CentreAction";
-import { click } from "ol/events/condition";
-import { getSelectedSegment } from "../../../../Mapping/Features/FrontFlow/FrontStyles";
+	translateOff,
+	translateOn,
+	endDrawing,
+	selectOff,
+	selectOn,
+	zoomingInAndCenter,
+	zoomingOutAndCenter,
+	dragPanOff,
+	save,
+	dragPanOn,
+} from '../../../../Mapping/Map';
+import { cloudDrawingON } from '../../../../Mapping/Features/Clouds/Clouds';
+import { frontFlowDrawingON } from '../../../../Mapping/Features/FrontFlow/FrontFlow';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMapCoordinate, setModal, setOption } from '../redux/actions';
+import { jetDrawingON } from '../../../../Mapping/Features/Jet/Jet';
+import { centreActionDrawingON } from '../../../../Mapping/Features/CentreAction/CentreAction';
+import { CATDrawingON } from 'Mapping/Features/CAT/CAT';
 
 function Tools() {
-  const map = useSelector((state) => state.map);
-  const modal = useSelector((state) => state.modal);
-  const option = useSelector((state) => state.option);
-  const mapCoordinate = useSelector((state) => state.mapCoordinate);
-  const selectedFeature = useSelector((state) => state.selectedFeature);
+	const map = useSelector((state) => state.map);
+	const modal = useSelector((state) => state.modal);
+	const option = useSelector((state) => state.option);
+	const mapCoordinate = useSelector((state) => state.mapCoordinate);
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const [undoRedo, setUndoRedo] = useState(null);
+	const [undoRedo, setUndoRedo] = useState(null);
 
-  const doubleClick = useCallback(
-    (event) => {
-      dispatch(setModal(""));
-      map.forEachFeatureAtPixel(
-        map.getEventPixel(event),
-        (feature) => {
-          if (feature.getGeometry().getType() !== "Point") {
-            dispatch(setModal(feature.get("feature_type")));
-          }
-        },
-        { hitTolerance: 10 }
-      );
-    },
-    [dispatch, map]
-  );
+	const doubleClick = useCallback(
+		(event) => {
+			console.log('double click');
+			dispatch(setModal(''));
+			map.forEachFeatureAtPixel(
+				map.getEventPixel(event),
+				(feature) => {
+					if (feature.getGeometry().getType() !== 'Point') {
+						dispatch(setModal(feature.get('feature_type')));
+						dispatch(setOption(''));
+					}
+				},
+				{ hitTolerance: 10 }
+			);
+		},
+		[dispatch, map]
+	);
 
-  const click = useCallback(
-    (event) => {
-      dispatch(setMapCoordinate(event.coordinate));
-      dispatch(setModal("centre_action"));
-      map.un("click", click);
-    },
-    [dispatch, map]
-  );
+	const click = useCallback(
+		(event) => {
+			dispatch(setMapCoordinate(event.coordinate));
+			dispatch(setModal('centre_action'));
+			dispatch(setOption(''));
+		},
+		[dispatch]
+	);
 
-  const nothing = useCallback(() => {
-    if (map) {
-      map.un("singleclick", zoomingInAndCenter);
-      map.un("singleclick", zoomingOutAndCenter);
-      map.un("click", click);
-      map.getViewport().removeEventListener("dblclick", doubleClick);
+	useEffect(() => {
+		if (map) {
+			dragPanOff(map);
+			const ur = new UndoRedo({
+				maxLength: 10,
+			});
+			setUndoRedo(ur);
+			map.addInteraction(ur);
+		}
+	}, [map]);
 
-      dragPanOff(map);
-      endDrawing(map);
-      selectOff(map);
-      translateOff(map);
-      document.querySelector("#map-container").style.cursor = "unset";
-    }
-  }, [doubleClick, click, map]);
+	useEffect(() => {
+		if (map) {
+			switch (option) {
+				case 'zoom_in':
+					map.on('singleclick', zoomingInAndCenter);
+					document.querySelector('#map-container').style.cursor = 'zoom-in';
+					break;
+				case 'zoom_out':
+					map.on('singleclick', zoomingOutAndCenter);
+					document.querySelector('#map-container').style.cursor = 'zoom-out';
+					break;
+				case 'drag':
+					dragPanOn(map);
+					translateOn(map);
+					document.querySelector('#map-container').style.cursor = 'grab';
+					break;
+				case 'zone_texte':
+					console.log('not defined yet');
+					break;
+				case 'zone_nuageuse':
+					cloudDrawingON(map);
+					break;
+				case 'jet':
+					jetDrawingON(map);
+					break;
+				case 'courant_front':
+					frontFlowDrawingON(map);
+					break;
+				case 'CAT':
+					CATDrawingON(map);
+					break;
+				case 'ligne':
+					console.log('not defined yet');
+					break;
+				case 'fleche':
+					console.log('not defined yet');
+					break;
+				case 'centres_action':
+					if (!mapCoordinate) map.on('click', click);
+					centreActionDrawingON(map);
+					break;
+				case 'volcan':
+					console.log('not defined yet');
+					break;
+				case 'tropopause':
+					console.log('not defined yet');
+					break;
+				case 'condition_en_surface':
+					console.log('not defined yet');
+					break;
+				case 'select':
+					selectOn(map);
+					map.getViewport().addEventListener('dblclick', doubleClick);
+					break;
+				default:
+					break;
+			}
+		}
+		return () => {
+			if (map) {
+				map.getViewport().removeEventListener('dblclick', doubleClick);
+				map.un('click', click);
+				map.un('singleclick', zoomingInAndCenter);
+				map.un('singleclick', zoomingOutAndCenter);
+				dragPanOff(map);
+				endDrawing(map);
+				selectOff(map);
+				translateOff(map);
+				document.querySelector('#map-container').style.cursor = 'unset';
+			}
+		};
+	}, [doubleClick, click, map, option, mapCoordinate]);
 
-  const toggleToolsOption = useCallback(
-    (drawingFunction) => {
-      nothing();
-      drawingFunction(map);
-    },
-    [map, nothing]
-  );
+	const items = [
+		{
+			id: 'zoom_in',
+			icon: '/Icons/Clouds/magnifying-glass-plus-solid.svg',
+			alt: 'Zoom In icon',
+			title: 'zoom_in',
+			command: () => {
+				option !== 'zoom_in'
+					? dispatch(setOption('zoom_in'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'zoom_out',
+			icon: '/Icons/Clouds/magnifying-glass-minus-solid.svg',
+			alt: 'Zoom Out icon',
+			title: 'zoom_out',
+			command: () => {
+				option !== 'zoom_out'
+					? dispatch(setOption('zoom_out'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'drag',
+			icon: '/Icons/Clouds/hand-solid.svg',
+			alt: 'Drag icon',
+			title: 'drag',
+			command: () => {
+				option !== 'drag'
+					? dispatch(setOption('drag'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'zone_texte',
+			icon: '/Icons/Clouds/message-regular.svg',
+			alt: 'Zone de texte icon',
+			title: 'Zone de texte',
+			command: () => {
+				option !== 'zone_texte'
+					? dispatch(setOption('zone_texte'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'zone_nuageuse',
+			icon: '/Icons/Clouds/cloud-solid.svg',
+			alt: 'Zone nuageuse icon',
+			title: 'Zone nuageuse',
+			command: () => {
+				option !== 'zone_nuageuse'
+					? dispatch(setOption('zone_nuageuse'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'jet',
+			icon: '/Icons/Clouds/wind-solid.svg',
+			alt: 'Courant jet icon',
+			title: 'Courant jet',
+			command: () => {
+				option !== 'jet'
+					? dispatch(setOption('jet'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'courant_front',
+			icon: '/Icons/Front/graph.png',
+			alt: 'front icon',
+			title: 'Courant front',
+			command: () => {
+				option !== 'courant_front'
+					? dispatch(setOption('courant_front'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'CAT',
+			icon: '/Icons/CAT/dashed-line.png',
+			alt: 'Cat icon',
+			title: 'CAT',
+			command: () => {
+				option !== 'CAT'
+					? dispatch(setOption('CAT'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'ligne',
+			icon: '/Icons/Clouds/i-cursor-solid.svg',
+			alt: 'ligne icon',
+			title: 'Ligne',
+			command: () => {
+				option !== 'ligne'
+					? dispatch(setOption('ligne'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'fleche',
+			icon: '/Icons/Clouds/i-cursor-solid.svg',
+			alt: 'Flèche icon',
+			title: 'Flèche',
+			command: () => {
+				option !== 'fleche'
+					? dispatch(setOption('fleche'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'centres_action',
+			icon: '/Icons/Clouds/centre_action.png',
+			alt: "Centres d'action icon",
+			title: "Centres d'action",
+			command: () => {
+				option !== 'centres_action'
+					? dispatch(setOption('centres_action'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'volcan',
+			icon: '/Icons/Clouds/volcano-solid.svg',
+			alt: 'Volcan icon',
+			title: 'Volcan',
+			command: () => {
+				option !== 'volcan'
+					? dispatch(setOption('volcan'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'tropopause',
+			icon: '/Icons/Clouds/i-cursor-solid.svg',
+			alt: 'tropopause icon',
+			title: 'Tropopause',
+			command: () => {
+				option !== 'tropopause'
+					? dispatch(setOption('tropopause'))
+					: dispatch(setOption('select'));
+			},
+		},
+		{
+			id: 'condition_en_surface',
+			icon: '/Icons/Clouds/flag.png',
+			alt: 'text Zone icon',
+			title: 'Condition en surface',
+			command: () => {
+				option !== 'condition_en_surface'
+					? dispatch(setOption('condition_en_surface'))
+					: dispatch(setOption('select'));
+			},
+		},
+	];
 
-  const dragAndTranslate = useCallback(() => {
-    nothing();
-    map.getInteractions().forEach((interaction) => {
-      if (interaction instanceof DragPan) {
-        interaction.setActive(true);
-      }
-    });
-    translateOn(map);
-    document.querySelector("#map-container").style.cursor = "grab";
-  }, [map, nothing]);
-
-  const zoom = useCallback(
-    (opt, action) => {
-      nothing();
-      map.on("singleclick", action);
-      document.querySelector("#map-container").style.cursor =
-        opt === "zoom_in" ? "zoom-in" : "zoom-out";
-    },
-    [map, nothing]
-  );
-  useEffect(() => {
-    if (map) {
-      const ur = new UndoRedo({
-        maxLength: 10,
-      });
-      setUndoRedo(ur);
-      map.addInteraction(ur);
-    }
-  }, [map]);
-
-  useEffect(() => {
-    if (map) {
-      switch (option) {
-        case "zoom_in":
-          zoom("zoom_in", zoomingInAndCenter);
-          break;
-        case "zoom_out":
-          zoom("zoom_out", zoomingOutAndCenter);
-          break;
-        case "drag":
-          dragAndTranslate();
-          break;
-        case "zone_texte":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "zone_nuageuse":
-          toggleToolsOption(cloudDrawingON);
-          break;
-        case "jet":
-          toggleToolsOption(jetDrawingON);
-          break;
-        case "courant_front":
-          toggleToolsOption(frontFlowDrawingON);
-          break;
-        case "cat":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "ligne":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "fleche":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "centres_action":
-          toggleToolsOption(centreActionDrawingON);
-          map.on("click", click);
-          break;
-        case "volcan":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "tropopause":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "condition_en_surface":
-          toggleToolsOption(jetFlowDrawingON);
-          break;
-        case "select":
-          toggleToolsOption(selectOn);
-          map.getViewport().addEventListener("dblclick", doubleClick);
-          break;
-        default:
-          nothing();
-          break;
-      }
-    }
-  }, [
-    doubleClick,
-    click,
-    dragAndTranslate,
-    map,
-    option,
-    selectedFeature,
-    toggleToolsOption,
-    zoom,
-  ]);
-  const items = [
-    {
-      id: "zoom_in",
-      icon: "/Icons/Clouds/magnifying-glass-plus-solid.svg",
-      alt: "Zoom In icon",
-      title: "zoom_in",
-      command: () => {
-        option !== "zoom_in"
-          ? dispatch(setOption("zoom_in"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "zoom_out",
-      icon: "/Icons/Clouds/magnifying-glass-minus-solid.svg",
-      alt: "Zoom Out icon",
-      title: "zoom_out",
-      command: () => {
-        option !== "zoom_out"
-          ? dispatch(setOption("zoom_out"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "drag",
-      icon: "/Icons/Clouds/hand-solid.svg",
-      alt: "Drag icon",
-      title: "drag",
-      command: () => {
-        option !== "drag"
-          ? dispatch(setOption("drag"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "zone_texte",
-      icon: "/Icons/Clouds/message-regular.svg",
-      alt: "Zone de texte icon",
-      title: "Zone de texte",
-      command: () => {
-        option !== "zone_texte"
-          ? dispatch(setOption("zone_texte"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "zone_nuageuse",
-      icon: "/Icons/Clouds/cloud-solid.svg",
-      alt: "Zone nuageuse icon",
-      title: "Zone nuageuse",
-      command: () => {
-        option !== "zone_nuageuse"
-          ? dispatch(setOption("zone_nuageuse"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "jet",
-      icon: "/Icons/Clouds/wind-solid.svg",
-      alt: "Courant jet icon",
-      title: "Courant jet",
-      command: () => {
-        option !== "jet"
-          ? dispatch(setOption("jet"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "courant_front",
-      icon: "/Icons/Clouds/i-cursor-solid.svg",
-      alt: "front icon",
-      title: "Courant front",
-      command: () => {
-        option !== "courant_front"
-          ? dispatch(setOption("courant_front"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "cat",
-      icon: "/Icons/Clouds/i-cursor-solid.svg",
-      alt: "Cat icon",
-      title: "Cat",
-      command: () => {
-        option !== "cat"
-          ? dispatch(setOption("cat"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "ligne",
-      icon: "/Icons/Clouds/i-cursor-solid.svg",
-      alt: "ligne icon",
-      title: "Ligne",
-      command: () => {
-        option !== "ligne"
-          ? dispatch(setOption("ligne"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "fleche",
-      icon: "/Icons/Clouds/i-cursor-solid.svg",
-      alt: "Flèche icon",
-      title: "Flèche",
-      command: () => {
-        option !== "fleche"
-          ? dispatch(setOption("fleche"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "centres_action",
-      icon: "/Icons/Clouds/centre_action.png",
-      alt: "Centres d'action icon",
-      title: "Centres d'action",
-      command: () => {
-        option !== "centres_action"
-          ? dispatch(setOption("centres_action"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "volcan",
-      icon: "/Icons/Clouds/volcano-solid.svg",
-      alt: "Volcan icon",
-      title: "Volcan",
-      command: () => {
-        option !== "volcan"
-          ? dispatch(setOption("volcan"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "tropopause",
-      icon: "/Icons/Clouds/i-cursor-solid.svg",
-      alt: "tropopause icon",
-      title: "Tropopause",
-      command: () => {
-        option !== "tropopause"
-          ? dispatch(setOption("tropopause"))
-          : dispatch(setOption("select"));
-      },
-    },
-    {
-      id: "condition_en_surface",
-      icon: "/Icons/Clouds/flag.png",
-      alt: "text Zone icon",
-      title: "Condition en surface",
-      command: () => {
-        option !== "condition_en_surface"
-          ? dispatch(setOption("condition_en_surface"))
-          : dispatch(setOption("select"));
-      },
-    },
-  ];
-
-  return (
-    <div className="tools">
-      <button
-        onClick={() => {
-          save(map);
-        }}
-        id={"save"}
-        title="Sauvgarder"
-      >
-        <img
-          src="/Icons/Clouds/floppy-disk-solid.svg"
-          alt="save"
-          height={20}
-          width={20}
-        />
-      </button>
-      <button
-        onClick={() => {
-          undoRedo.undo();
-        }}
-        id={"save"}
-        title="Undo"
-      >
-        <img
-          src="/Icons/Clouds/rotate-left-solid.svg"
-          alt="undo"
-          height={20}
-          width={20}
-        />
-      </button>
-      <button
-        onClick={() => {
-          undoRedo.redo();
-        }}
-        id={"save"}
-        title="Redo"
-      >
-        <img
-          src="/Icons/Clouds/rotate-right-solid.svg"
-          alt="redo"
-          height={20}
-          width={20}
-        />
-      </button>
-      {items.map((item, key) => (
-        <button
-          onClick={item.command}
-          key={key}
-          id={item.id}
-          disabled={modal !== ""}
-          className={option === item.id ? "active" : ""}
-          title={item.title}
-        >
-          <img src={item.icon} alt={item.alt} height={20} width={20} />
-        </button>
-      ))}
-    </div>
-  );
+	return (
+		<div className="tools">
+			<button
+				onClick={() => {
+					save(map);
+				}}
+				id={'save'}
+				title="Sauvgarder">
+				<img
+					src="/Icons/Clouds/floppy-disk-solid.svg"
+					alt="save"
+					height={20}
+					width={20}
+				/>
+			</button>
+			<button
+				onClick={() => {
+					undoRedo.undo();
+				}}
+				id={'save'}
+				title="Undo">
+				<img
+					src="/Icons/Clouds/rotate-left-solid.svg"
+					alt="undo"
+					height={20}
+					width={20}
+				/>
+			</button>
+			<button
+				onClick={() => {
+					undoRedo.redo();
+				}}
+				id={'save'}
+				title="Redo">
+				<img
+					src="/Icons/Clouds/rotate-right-solid.svg"
+					alt="redo"
+					height={20}
+					width={20}
+					onClick={test}
+				/>
+			</button>
+			{items.map((item, key) => (
+				<button
+					onClick={item.command}
+					key={key}
+					id={item.id}
+					disabled={modal !== ''}
+					className={option === item.id ? 'active' : ''}
+					title={item.title}>
+					<img src={item.icon} alt={item.alt} height={20} width={20} />
+				</button>
+			))}
+		</div>
+	);
 }
 
 export default Tools;
--- src/Pages/Production/Features/CentreAction/CentreAction.js
+++ src/Pages/Production/Features/CentreAction/CentreAction.js
@@ -1,108 +1,118 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 import {
-  centreActionVectorLayer,
-  createCentreAction,
-  modifyCentreAction,
-  selectCentreAction,
-  translateCentreAction,
-} from "../../../../Mapping/Features/CentreAction/CentreAction";
+	centreActionVectorLayer,
+	modifyCentreAction,
+	selectCentreAction,
+	translateCentreAction,
+} from 'Mapping/Features/CentreAction/CentreAction';
 import {
-  displayResizer,
-  createResizer,
-} from "../../../../Mapping/Features/CentreAction/Resizer";
-import * as extent from "ol/extent";
-import { useDispatch, useSelector } from "react-redux";
-import { endDrawing } from "Mapping/Map";
+	displayResizer,
+	createResizer,
+} from 'Mapping/Features/CentreAction/Resizer';
+import * as extent from 'ol/extent';
+import { useDispatch, useSelector } from 'react-redux';
+import { endDrawing } from 'Mapping/Map';
 import {
-  setOption,
-  setSelectedFeature,
-} from "../../CardDrawingTools/redux/actions";
-import Overlay from "ol/Overlay";
-import Resizer from "./Resizer/Resizer";
-import Window from "./Window/Window";
-import { Rnd } from "react-rnd";
+	setOption,
+	setSelectedFeature,
+	setCenterResizer,
+} from 'Pages/Production/CardDrawingTools/redux/actions';
+import Overlay from 'ol/Overlay';
+import Resizer from './Resizer/Resizer';
+import Window from './Window/Window';
+import { Rnd } from 'react-rnd';
 
 function CentreAction() {
-  const [resizeActive, setResizeActive] = useState(false);
-  const [top, setTop] = useState(0);
-  const [left, setLeft] = useState(0);
-  const [widthResizer, setWidthResizer] = useState(80);
-  const [heightResizer, setHeightResizer] = useState(80);
-  const [widthImg, setWidthImg] = useState(80);
-  const [heightImg, setHeightImg] = useState(100);
+	const [resizeActive, setResizeActive] = useState(false);
+	const [top, setTop] = useState(0);
+	const [left, setLeft] = useState(0);
+	const [widthResizer, setWidthResizer] = useState(80);
+	const [heightResizer, setHeightResizer] = useState(80);
+	const [widthImg, setWidthImg] = useState(80);
+	const [heightImg, setHeightImg] = useState(100);
 
-  const [nameCentre, setNameCentre] = useState("c");
-  const [vitesse, setVitesse] = useState(null);
-  const [texte, setTexte] = useState("");
-  const [direction, setDirection] = useState(0);
+	const [nameCentre, setNameCentre] = useState('c');
+	const [vitesse, setVitesse] = useState(null);
+	const [texte, setTexte] = useState('');
+	const [direction, setDirection] = useState(0);
 
-  const map = useSelector((state) => state.map);
-  const dispatch = useDispatch();
-  const modal = useSelector((state) => state.modal);
-  const mapCoordinate = useSelector((state) => state.mapCoordinate);
-  const selectedFeature = useSelector((state) => state.selectedFeature);
+	const map = useSelector((state) => state.map);
+	const dispatch = useDispatch();
+	const modal = useSelector((state) => state.modal);
+	const mapCoordinate = useSelector((state) => state.mapCoordinate);
+	const selectedFeature = useSelector((state) => state.selectedFeature);
+	const centerResizer = useSelector((state) => state.centerResizer);
 
-  const init = useCallback(() => {
-    const feature = createResizer(map, mapCoordinate);
-    dispatch(setSelectedFeature(feature));
-  }, [dispatch, map]);
+	const init = useCallback(() => {
+		let resizer = createResizer(map, mapCoordinate);
+		dispatch(setCenterResizer(resizer));
+	}, [dispatch, map, mapCoordinate]);
 
-  useEffect(() => {
-    if (map) init();
-  }, [init, map]);
+	useEffect(() => {
+		if (map) init();
+	}, [init, map]);
 
-  useEffect(() => {
-    if (!mapCoordinate) return;
-    displayResizer(selectedFeature, mapCoordinate);
-  }, [dispatch, mapCoordinate]);
+	useEffect(() => {
+		if (!mapCoordinate || selectedFeature) return;
+		displayResizer(centerResizer, mapCoordinate);
+		let overlayElt = new Overlay({
+			element: null,
+			stopEvent: false,
+		});
+		overlayElt.set('feature_type', 'centre_action');
+		overlayElt.set('nameCentre', 'c');
+		overlayElt.set('vitesseCentre', 0);
+		overlayElt.set('texteCentre', '');
+		overlayElt.set('directionCentre', -1);
+		dispatch(setSelectedFeature(overlayElt));
+	}, [centerResizer, dispatch, mapCoordinate, selectedFeature]);
 
-  const resizerOverlayDiv = () => {
-    const display = modal === "centre_action" ? "block" : "none";
-    return (
-      <div id="resizer" style={{ display: { display } }}>
-        <Resizer
-          nameCentre={nameCentre}
-          vitesse={vitesse}
-          direction={direction}
-          texte={texte}
-          resizeActive={resizeActive}
-          setResizeActive={setResizeActive}
-          setWidthImg={setWidthImg}
-          setHeightImg={setHeightImg}
-          widthImg={widthImg}
-          heightImg={heightImg}
-          top={top}
-          left={left}
-          setTop={setTop}
-          setLeft={setLeft}
-          widthResizer={widthResizer}
-          setWidthResizer={setWidthResizer}
-          heightResizer={heightResizer}
-          setHeightResizer={setHeightResizer}
-        ></Resizer>
-      </div>
-    );
-  };
+	const resizerOverlayDiv = () => {
+		const display = modal === 'centre_action' ? 'block' : 'none';
+		return (
+			<div id="resizer" style={{ display: { display } }}>
+				<Resizer
+					nameCentre={nameCentre}
+					vitesse={vitesse}
+					direction={direction}
+					texte={texte}
+					resizeActive={resizeActive}
+					setResizeActive={setResizeActive}
+					setWidthImg={setWidthImg}
+					setHeightImg={setHeightImg}
+					widthImg={widthImg}
+					heightImg={heightImg}
+					top={top}
+					left={left}
+					setTop={setTop}
+					setLeft={setLeft}
+					widthResizer={widthResizer}
+					setWidthResizer={setWidthResizer}
+					heightResizer={heightResizer}
+					setHeightResizer={setHeightResizer}></Resizer>
+			</div>
+		);
+	};
 
-  return (
-    <div>
-      {resizerOverlayDiv()}{" "}
-      {modal === "centre_action" && (
-        <Rnd className="snapshotDialog" style={{ zIndex: 6 }}>
-          <Window
-            nameCentre={nameCentre}
-            setNameCentre={setNameCentre}
-            vitesse={vitesse}
-            setVitesse={setVitesse}
-            direction={direction}
-            setDirection={setDirection}
-            texte={texte}
-            setTexte={setTexte}
-          />
-        </Rnd>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			{resizerOverlayDiv()}{' '}
+			{modal === 'centre_action' && (
+				<Rnd className="snapshotDialog" style={{ zIndex: 6 }}>
+					<Window
+						nameCentre={nameCentre}
+						setNameCentre={setNameCentre}
+						vitesse={vitesse}
+						setVitesse={setVitesse}
+						direction={direction}
+						setDirection={setDirection}
+						texte={texte}
+						setTexte={setTexte}
+					/>
+				</Rnd>
+			)}
+		</div>
+	);
 }
 
 export default CentreAction;
